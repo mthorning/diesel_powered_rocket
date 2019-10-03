75f28429e10dfe55c26894afdf163c28d7ad7dd1
mthorning <matthewthorning@gmail.com>
2019-10-03 22:08:50 +0100

    added svelte for the frontend files

---
+++ svelte-demo/src/components/BlogForm.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let hideForm;
+    export let savePost;
+
+    let formValues = {
+	title: '',
+	body: '',
+    };
+
+    function submitHandler() {
+	savePost(formValues);
+	formValues = { title: '', body: '' };
+    }
+</script>
+
+<style>
+    div {
+	display: flex;
+	flex-direction: row;
+	align-items: baseline;
+	justify-content: space-evenly;
+	width: 400px;
+    }
+    .submit {
+	justify-content: flex-end;
+    }
+</style>
+
+<button on:click={hideForm}>Hide Form</button>
+
+<form on:submit|preventDefault={submitHandler}>
+    <div>
+	<label>Title</label>
+	<input bind:value={formValues.title} />
+    </div>
+    <div>
+	<label>Body</label>
+	<textarea bind:value={formValues.body} />
+    </div>
+    <div class="submit">
+	<input type="submit" />
+    </div>
+</form>
---
+++ svelte-demo/src/components/PostViewer.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let posts = [];
+
+    let selectedPost;
+</script>
+
+<style>
+    .container {
+	margin: 20px;
+	border: 1px solid gray;
+	border-radius: 4px;
+	padding: 20px;
+    }
+    h2 {
+	text-align: center;
+    }
+</style>
+
+<div class="container">
+<h2>Blog Posts</h2>
+{#if posts.length}
+    <select bind:value={selectedPost}>
+	{#each posts as post (post.id)}
+	    <option value={post.id}>{post.title}</option>
+	{/each}
+    </select>
+{:else}
+    <p>No posts.</p>
+{/if}
+
+{#if selectedPost}
+    <pre>{posts.find(post => post.id === selectedPost).body}</pre>
+{/if}
+</div>
